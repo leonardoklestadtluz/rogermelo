72fe5eab455b73f8f4b191f29d31157abc6a619f
leonardoklestadtluz <klestadt@hotmail.com>
2023-12-03 13:39:27 -0300

    conclusão da aula Resolvendo Bateria de Exercícios 02

--- bateria-de-exercicios-02/app.js
+++ bateria-de-exercicios-02/app.js
@@ -56,7 +56,7 @@ const serie = {
   temaAbertura: 'I\'ll Be There for You',
   elencoPrincipla: ['Jennifer Aniston', 'Courteney Cox', 'Lisa Kudrow', 'Matt LeBlanc', 'Matthew Perry', 'David Schwimmer']
 }
-//console.log(serie)
+// console.log(serie)
 
 
 /*
@@ -209,8 +209,8 @@ const pokemon = {
     }
   ]
 }
-const primeiroCaracter = pokemon.moves[0].versionGroupDetails[1].versionGroup.url[0]
-// console.log(primeiroCaracter)
+const primeiroCaracterUrl = pokemon.moves[0].versionGroupDetails[1].versionGroup.url[0]
+// console.log(primeiroCaracterUrl)
 
 
 /*
@@ -245,4 +245,4 @@ const propriedade = pokemon.moves[0].versionGroupDetails[1].moveLearnMethod.name
 
 const paragrafo = `Nesta bateria de exercícios vimos que marca armazena um array no qual o segundo item é ${marcas[1]}, um dos gêneros da banda ${banda.nome} é ${banda.generos[3]} e que a propriedade moveLearnMethod do Articuno contém um objeto com uma propriedade name que armazena "${propriedade}".`
 
-console.log(paragrafo)
+ console.log(paragrafo)
